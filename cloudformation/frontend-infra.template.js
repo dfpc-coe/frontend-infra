fb60f00fd66b0e0e17b19f62810008ca5c09cf43
ingalls <nick@ingalls.ca>
2024-03-21 13:51:48 -0600

    Add DB Export

---
+++ cloudformation/frontend-infra.template.js
@@ -0,0 +1,20 @@
+import cf from '@openaddresses/cloudfriend';
+import RDS from './lib/db.js';
+import KMS from './lib/kms.js';
+
+export default cf.merge(
+    {
+        Description: 'Template for @tak-ps/frontend-infra',
+        Parameters: {
+            GitSha: {
+                Description: 'GitSha that is currently being deployed',
+                Type: 'String'
+            },
+            Environment: {
+                Description: 'VPC/ECS Stack to deploy into',
+                Type: 'String',
+                Default: 'prod'
+            },
+        },
+    }, RDS, KMS
+);
---
+++ cloudformation/lib/db.js
@@ -0,0 +1,129 @@
+import cf from '@openaddresses/cloudfriend';
+
+export default {
+    Parameters: {
+        DatabaseType: {
+            Type: 'String',
+            Default: 'db.t3.micro',
+            Description: 'Database size to create',
+            AllowedValues: [
+                'db.t3.micro'
+            ]
+        }
+    },
+    Resources: {
+        DBMasterSecret: {
+            Type: 'AWS::SecretsManager::Secret',
+            Properties: {
+                Description: cf.join([cf.stackName, ' RDS Master Password']),
+                GenerateSecretString: {
+                    SecretStringTemplate: '{"username": "frontend"}',
+                    GenerateStringKey: 'password',
+                    ExcludePunctuation: true,
+                    PasswordLength: 32
+                },
+                Name: cf.join([cf.stackName, '/rds/secret']),
+                KmsKeyId: cf.ref('KMS')
+            }
+        },
+        DBMasterSecretAttachment: {
+            Type: 'AWS::SecretsManager::SecretTargetAttachment',
+            Properties: {
+                SecretId: cf.ref('DBMasterSecret'),
+                TargetId: cf.ref('DBInstance'),
+                TargetType: 'AWS::RDS::DBInstance'
+            }
+        },
+        DBMonitoringRole: {
+            Type: 'AWS::IAM::Role',
+            Properties: {
+                AssumeRolePolicyDocument: {
+                    Version: '2012-10-17',
+                    Statement: [{
+                        Sid: '',
+                        Effect: 'Allow',
+                        Principal: {
+                            Service: 'monitoring.rds.amazonaws.com'
+                        },
+                        Action: 'sts:AssumeRole'
+                    }]
+                },
+                ManagedPolicyArns: [
+                    cf.join(['arn:', cf.partition, ':iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole'])
+                ],
+                Path: '/'
+            }
+        },
+        DBInstance: {
+            Type: 'AWS::RDS::DBInstance',
+            DependsOn: ['DBMasterSecret'],
+            Properties: {
+                Engine: 'MySQL',
+                AllowMajorVersionUpgrade: false,
+                DBName: 'frontend',
+                CopyTagsToSnapshot: true,
+                DBInstanceIdentifier: cf.stackName,
+                MonitoringInterval: 60,
+                MonitoringRoleArn: cf.getAtt('DBMonitoringRole', 'Arn'),
+                KmsKeyId: cf.ref('KMS'),
+                EngineVersion: '8.0.33',
+                StorageEncrypted: true,
+                MasterUsername: cf.sub('{{resolve:secretsmanager:${AWS::StackName}/rds/secret:SecretString:username:AWSCURRENT}}'),
+                MasterUserPassword: cf.sub('{{resolve:secretsmanager:${AWS::StackName}/rds/secret:SecretString:password:AWSCURRENT}}'),
+                EnablePerformanceInsights: true,
+                PerformanceInsightsKMSKeyId: cf.ref('KMS'),
+                PerformanceInsightsRetentionPeriod: 7,
+                AllocatedStorage: 50,
+                MaxAllocatedStorage: 100,
+                BackupRetentionPeriod: 10,
+                StorageType: 'gp3',
+                DBInstanceClass: cf.ref('DatabaseType'),
+                VPCSecurityGroups: [cf.ref('DBVPCSecurityGroup')],
+                DBSubnetGroupName: cf.ref('DBSubnet'),
+                PubliclyAccessible: true,
+                DeletionProtection: true
+            }
+        },
+        DBSubnet: {
+            Type: 'AWS::RDS::DBSubnetGroup',
+            Properties: {
+                DBSubnetGroupDescription: cf.join('-', [cf.stackName, 'rds-subnets']),
+                SubnetIds: [
+                    cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-subnet-private-a'])),
+                    cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-subnet-private-b']))
+                ]
+            }
+        },
+        DBVPCSecurityGroup: {
+            Type: 'AWS::EC2::SecurityGroup',
+            Properties: {
+                Tags: [{
+                    Key: 'Name',
+                    Value: cf.join('-', [cf.stackName, 'rds-sg'])
+                }],
+                GroupName: cf.join('-', [cf.stackName, 'rds-sg']),
+                GroupDescription: 'Allow RDS Database Ingress',
+                VpcId: cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-vpc'])),
+                SecurityGroupIngress: [{
+                    IpProtocol: '-1',
+                    SourceSecurityGroupId: cf.getAtt('ServiceSecurityGroup', 'GroupId')
+                },{
+                    IpProtocol: '-1',
+                    CidrIp: '0.0.0.0/0'
+                }]
+            }
+        }
+    },
+    Outputs: {
+        DB: {
+            Description: 'MySQL Connection String',
+            Value: cf.join([
+                'Server=', cf.getAtt('DBInstance', 'Endpoint.Address'), '; ',
+                'Port=3306; ',
+                'Database=frontend; ',
+                'Uid=', cf.sub('{{resolve:secretsmanager:${AWS::StackName}/rds/secret:SecretString:username:AWSCURRENT}}'), '; ',
+                'Pwd=', cf.sub('{{resolve:secretsmanager:${AWS::StackName}/rds/secret:SecretString:password:AWSCURRENT}}'), '; ',
+            ])
+        }
+    }
+};
